9b1a3ed5f11debb8144be4de279e2284d956e202
nguyentri61 <nguyenphanminhtri612004@gmail.com>
2024-03-14 18:39:57 +0700

    Update menu User/Tho main

--- DoAnTGVL/obj/Debug/net6.0-windows/DoAnTGVL.AssemblyInfo.cs
+++ DoAnTGVL/obj/Debug/net6.0-windows/DoAnTGVL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnTGVL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8572f1f690537f41f9c308940c746582b58bfb54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079397d319bc3f131b93fc15c27894a7dddabe37")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnTGVL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnTGVL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
