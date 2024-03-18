d71c242b929bc3b4734c3b86fb0a5494a7b61835
HuynhDan <vdan2242004@gmail.com>
2024-03-18 10:59:12 +0700

    DeMo

---
+++ DoAnTGVL/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DoAnTGVL.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã Thêm"); 
+        }
+    }
+}
